05a1dd0ed11e3495e2faf119ab480159e9048b7b
cemister <dosksys0@gmail.com>
2023-07-22 00:09:37 +0300

    Update in handling YAML path, see release notes

--- main.go
+++ main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"runtime"
 	"scaffolder/helper"
 	"scaffolder/utils"
 )
 
 func main() {
-	// Project name and path to YAML
+	// Project name
 	var name, yaml string
 	// Initialize Git?
 	var git bool
 
+	var yamlPath string
+
 	// Define and parse flags
 	flag.StringVar(&name, "name", "", "Project name")
-	flag.StringVar(&yaml, "yaml", "", "Path to YAML with directory structure")
+	flag.StringVar(&yaml, "yaml", "", "Config to use")
 	flag.BoolVar(&git, "git", false, "Use git in project")
 	flag.Parse()
 
@@ -28,7 +33,16 @@ func main() {
 		helper.Git(name)
 	}
 
+	// Construct a path to the provided yaml config file
+	if runtime.GOOS == "windows" {
+		yamlPath = os.Getenv("USERPROFILE") + "\\scaffolder-configs\\" + yaml + ".yaml"
+	} else {
+		home, err := os.UserHomeDir()
+		helper.Fatal(fmt.Sprintf("Failed to get home directory: %s", err), true, err)
+		yamlPath = home + "/scaffolder-configs/" + yaml + ".yaml"
+	}
+
 	// Scaffold the directory structure
-	utils.Scaffold(name, yaml)
+	utils.Scaffold(name, yamlPath)
 
 }
