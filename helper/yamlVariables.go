b9ffbad4c436e37688081f2c44202176c32e4b67
Gwali <gwalisam37@gmail.com>
2023-07-23 19:54:37 +0000

    updated readme and moved yamlVariables to helpers package

---
+++ helper/yamlVariables.go
@@ -0,0 +1,35 @@
+package helper
+
+import (
+	"fmt"
+	"strings"
+)
+
+type YamlVariableMap map[string]string
+
+func (y *YamlVariableMap) String() string {
+	output := []string{}
+
+	for k, v := range *y {
+		output = append(output, fmt.Sprintf("%s:%s", k, v))
+	}
+	return strings.Join(output, ",")
+
+}
+
+func (y *YamlVariableMap) Set(val string) error {
+	values := strings.Split(val, ",")
+	for _, v := range values {
+		setVariable := strings.Split(v, ":")
+		if len(setVariable) < 2 {
+			Fatal("Invalid format for varibale assignment", false)
+			return fmt.Errorf("Invalid format for key-value pair: %s", v)
+		}
+		key := setVariable[0]
+		valu := setVariable[1]
+		(*y)[key] = valu
+
+	}
+
+	return nil
+}
